ae3c68d4bd9009d2cb6bcc2b66047d810e5d86a8
Eugeniu Nicolenco <eugeniu.nicolenco@tekoway.com>
2020-03-28 18:21:43 +0200

    finished level 4

--- symfony/src/Controller/HomeController.php
+++ symfony/src/Controller/HomeController.php
@@ -20,9 +20,6 @@ class HomeController extends AbstractController
     public function __invoke()
     {
 
-        $x = 1;
-
-        return new Response('');
         return new Response($this->solver->solveLast());
 //        return new JsonResponse(['response' => $this->solver->solve()]);
     }

d47316d5dc2877d4aadb2bf008d0781d407ea89b
Eugeniu Nicolenco <eugeniu.nicolenco@tekoway.com>
2020-03-31 12:34:53 +0300

    finished level 3

---
+++ symfony/src/Service/Entity/Earning.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace App\Entity\Service;
+
+
+class Earning
+{
+    /** @var int */
+    protected $amount;
+    /** @var string */
+    protected $destination;
+
+    /** @var int */
+    protected $day;
+
+    /** @var int */
+    protected $id;
+
+    /**
+     * @var bool
+     */
+    protected $active = true;
+
+    public function __construct($destination = null, $day = null, $amount = null)
+    {
+        $this->destination = $destination;
+        $this->day = $day;
+        $this->amount = $amount;
+    }
+
+    public function __toString(): string
+    {
+        return $this->destination ?? "";
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return Earning
+     */
+    public function setId(int $id): Earning
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getAmount(): int
+    {
+        return $this->amount;
+    }
+
+    /**
+     * @param int $amount
+     *
+     * @return Earning
+     */
+    public function setAmount(int $amount): Earning
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getDay(): ?int
+    {
+        return $this->day;
+    }
+
+    /**
+     * @param int $day
+     *
+     * @return Earning
+     */
+    public function setDay(int $day): Earning
+    {
+        $this->day = $day;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDestination(): string
+    {
+        return $this->destination;
+    }
+
+    /**
+     * @param string $destination
+     *
+     * @return Earning
+     */
+    public function setDestination(string $destination): Earning
+    {
+        $this->destination = $destination;
+
+        return $this;
+    }
+
+    /**
+     * @param bool $active
+     *
+     * @return Earning
+     */
+    public function setActive(bool $active): Earning
+    {
+        $this->active = $active;
+
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isActive(): bool
+    {
+        return $this->active;
+    }
+
+    /**
+     * @param Earning[] $earnings
+     * @param string $parameter
+     */
+    public static function orderBy(&$earnings, $parameter = 'day')
+    {
+        $parameter = ucfirst($parameter);
+        try {
+            $e = (new self())->{'get'. $parameter}();
+        } catch (\Exception $exception) {
+            return;
+        }
+
+        $c = count($earnings);
+        for ($i = 0; $i < $c - 1; $i++) {
+            for ($j = $i + 1; $j < $c; $j++) {
+                if ($earnings[$i]->{'get'. $parameter}() > $earnings[$j]->{'get'. $parameter}()) {
+                    [$earnings[$i], $earnings[$j]] = [$earnings[$j], $earnings[$i]];
+                }
+            }
+        }
+    }
+}
---
+++ symfony/src/Service/Entity/Pair.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Entity\Service;
+
+class Pair
+{
+    protected $x;
+    protected $y;
+    protected $i;
+    protected $j;
+
+    public function __construct($x = null, $y = null, $i = null, $j = null)
+    {
+        $this->x = $x;
+        $this->y = $y;
+        $this->i = $i;
+        $this->j = $j;
+    }
+
+    /**
+     * @return null
+     */
+    public function getX()
+    {
+        return $this->x;
+    }
+
+    /**
+     * @param null $x
+     *
+     * @return Pair
+     */
+    public function setX($x): Pair
+    {
+        $this->x = $x;
+
+        return $this;
+    }
+
+    /**
+     * @return null
+     */
+    public function getY()
+    {
+        return $this->y;
+    }
+
+    /**
+     * @param null $y
+     *
+     * @return Pair
+     */
+    public function setY($y): Pair
+    {
+        $this->y = $y;
+
+        return $this;
+    }
+
+    /**
+     * @param Pair[]|array $array
+     */
+    public static function sortByX(&$array)
+    {
+        $c = count($array);
+        for ($i = 0; $i < ($c - 1); $i++) {
+            for($j = $i + 1; $j < $c; $j++) {
+                if ($array[$i]->getX() > $array[$j]->getX()) {
+                    [$array[$i], $array[$j]] = [$array[$j], $array[$i]];
+                }
+            }
+        }
+    }
+
+    /**
+     * @return null
+     */
+    public function getI()
+    {
+        return $this->i;
+    }
+
+    /**
+     * @return null
+     */
+    public function getJ()
+    {
+        return $this->j;
+    }
+
+    public function __toString()
+    {
+        return "{$this->x} {$this->y}";
+    }
+
+    /**
+     * @return array
+     */
+    public function toArray()
+    {
+        return [
+            $this->getX(), $this->getI(), $this->getY(), $this->getJ()
+        ];
+    }
+}
